db266d33814bbfb9a0d599b1bb61d2ccca72384e
Zoe Mazurkiewicz <zzmazurk@mtu.edu>
2023-02-03 15:18:50 -0500

    Zoe Testing

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo" /> //Zoe Test
         <p>
           Edit <code>src/App.js</code> and save to reload. --Josh Test
         </p>
