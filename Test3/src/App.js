cf9f209d43c74ee21566da2ea4d703e4f310ef31
Josh Hoefs <jehoefs@mtu.edu>
2023-03-01 12:03:02 -0500

    Started  available course list based on year/semester

--- Test3/src/App.js
+++ Test3/src/App.js
@@ -3,18 +3,73 @@ import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
 import uuid from "uuid/v4";
 import './App.css';
 
-const itemsFromBackend = [
-  { id: uuid(), content: "Intro to CS" },
-  { id: uuid(), content: "Calculus" },
+const firstYearFallCourses = [
+  { id: uuid(), content: "Intro to Programming I" },
+  { id: uuid(), content: "Calculus I" },
   { id: uuid(), content: "Composition" },
   { id: uuid(), content: "Intro to Chemistry" },
-  { id: uuid(), content: "Elective" }
+  { id: uuid(), content: "Technical Elective" }
+];
+
+const firstYearSpringCourses = [
+  { id: uuid(), content: "Intro to Programming II" },
+  { id: uuid(), content: "Calculus II" },
+  { id: uuid(), content: "Global Issues" },
+  { id: uuid(), content: "Discrete Structures" },
+  { id: uuid(), content: "Co-Curricular" }
+];
+
+const secondYearFallCourses = [
+  { id: uuid(), content: "Data Structures" },
+  { id: uuid(), content: "HW/SW Interface Programming" },
+  { id: uuid(), content: "Formal Models of Computation" },
+  { id: uuid(), content: "Statistical Methods" },
+  { id: uuid(), content: "HASS Elective" }
+];
+
+const secondYearSpringCourses = [
+  { id: uuid(), content: "Intro to Database Systems" },
+  { id: uuid(), content: "Team Software Project" },
+  { id: uuid(), content: "Elective Math" },
+  { id: uuid(), content: "HASS Elective" },
+  { id: uuid(), content: "Co-Curricular" }
+];
+
+const thirdYearFallCourses = [
+  { id: uuid(), content: "Concurrent Computing" },
+  { id: uuid(), content: "Computer Organization" },
+  { id: uuid(), content: "Ethical/Social Aspects of Computing" },
+  { id: uuid(), content: "CS Elective" },
+  { id: uuid(), content: "Co-Curricular" }
+];
+
+const thirdYearSpringCourses = [
+  { id: uuid(), content: "Systems Programming" },
+  { id: uuid(), content: "CS Elective" },
+  { id: uuid(), content: "HASS Elective" },
+  { id: uuid(), content: "Technical Elective" },
+  { id: uuid(), content: "Co-Curricular" }
+];
+
+const fourthYearFallCourses = [
+  { id: uuid(), content: "Intro to Algorithms" },
+  { id: uuid(), content: "CS Elective" },
+  { id: uuid(), content: "Technical Elective" },
+  { id: uuid(), content: "Co-curricular" }
+];
+
+const fourthYearSpringCourses = [
+  { id: uuid(), content: "Programming Languages" },
+  { id: uuid(), content: "Technical Elective" },
+  { id: uuid(), content: "CS Elective" },
+  { id: uuid(), content: "Technical and Professional Communication" },
+  { id: uuid(), content: "Technology and Society Elective" }
 ];
 
 const columnsFromBackend = {
   [uuid()]: {
     name: "Avaliable Courses",
-    items: itemsFromBackend
+    items: referenceCoursesByYearAndSemester("First Year", "Fall")
   },
  /* [uuid()]: {
     name: "Course Workspace",
@@ -66,8 +121,37 @@ const onDragEnd = (result, columns, setColumns) => {
 console.log(new Date().getFullYear());
 function clickClass(){
   console.log('click');
+  buildCourseByYearSelect("First Year", "FALL");
 }
 
+function referenceCoursesByYearAndSemester(year, semester){
+
+  if ((year == "First Year") & (semester == "Fall")) {
+    return firstYearFallCourses;
+  }
+  if ((year == "First Year") & (semester == "Spring")) {
+    return firstYearSpringCourses;
+  }
+  if ((year == "Second Year") & (semester == "Fall")) {
+    return secondYearFallCourses;
+  }
+  if ((year == "Second Year") & (semester == "Spring")) {
+    return secondYearSpringCourses;
+  }
+  if ((year == "Third Year") & (semester == "Fall")) {
+    return thirdYearFallCourses;
+  }
+  if ((year == "Third Year") & (semester == "Spring")) {
+    return thirdYearSpringCourses;
+  }
+  if ((year == "Fourth Year") & (semester == "Fall")) {
+    return fourthYearFallCourses;
+  }
+  if ((year == "Fourth Year") & (semester == "Spring")) {
+    return fourthYearSpringCourses;
+  }
+  
+}
 
 // This function is intended to build the list of classes offered for a particular semester and year
 function buildCourseByYearSelect(registrationClass, semester){
@@ -90,16 +174,22 @@ function buildCourseByYearSelect(registrationClass, semester){
   var year = new Date().getFullYear()
   console.log(year)
 
-  HTTPRequestURL = "https://api.michigantechcourses.com/courses?year=" + year + "&semester=" + semester
-  var HTTPRequest = new XMLHttpRequest()
-  var HTTPResponse;
+  var HTTPRequestURL = "https://api.michigantechcourses.com/courses?year=" + year + "&semester=" + semester;
+  var HTTPRequest = new XMLHttpRequest();
+  var HTTPResponse = "";
 
-  HTTPRequest.open("GET", HTTPRequestURL)
-  HTTPRequest.send
+  HTTPRequest.open("GET", HTTPRequestURL);
+  HTTPRequest.send;
 
-  if (HTTPRequest.responseText != null) {
-    HTTPResponse = HTTPRequest.reponseText
-    console.log(HTTPResponse)
+  console.log(HTTPRequestURL);
+  console.log(HTTPRequest);
+
+  if (HTTPRequest.responseText != "") {
+    HTTPResponse = HTTPRequest.reponseText;
+    
+  }
+  else {
+    console.log("empty HTTP response text");
   }
   
 }
