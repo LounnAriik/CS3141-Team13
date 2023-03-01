6ddadf8e35d718451f2006313f47ac7b29d299ad
Connor Ward <cfw2002@charter.net>
2023-03-01 11:20:09 -0500

    buildCourseByTransfer Request Retrieval Test

--- Test3/src/App.js
+++ Test3/src/App.js
@@ -65,6 +65,7 @@ const onDragEnd = (result, columns, setColumns) => {
 
 console.log(new Date().getFullYear());
 function clickClass(){
+  buildCourseByTransfer();
   console.log('click');
 }
 
@@ -104,6 +105,18 @@ function buildCourseByYearSelect(registrationClass, semester){
   
 }
 
+function buildCourseByTransfer() {
+  var collegeNames = [];
+  var RequestURL = "https://api.michigantechcourses.com/transfer-courses?updatedSince=2020-01-01T11%3A45%3A01.733Z";
+  var Request = new XMLHttpRequest();
+
+  Request.onload = function() {
+    console.log('Retrieved');
+  }
+
+  Request.open("GET", RequestURL);
+  Request.send();
+}
 
 
 function App() {
